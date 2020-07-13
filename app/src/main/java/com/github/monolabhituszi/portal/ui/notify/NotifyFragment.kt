1361074f2431a50c3168bdcb5ccceae6b653bdbe
Daishin16 <sakuya1202@icloud.com>
2020-07-13 14:54:59 +0900

    set Epoxy

--- app/src/main/java/com/github/monolabhituszi/portal/ui/notify/NotifyFragment.kt
+++ app/src/main/java/com/github/monolabhituszi/portal/ui/notify/NotifyFragment.kt
@@ -3,14 +3,17 @@ package com.github.monolabhituszi.portal.ui.notify
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.github.monolabhituszi.portal.R
 import com.github.monolabhituszi.portal.databinding.FragmentNotifyBinding
+import com.github.monolabhituszi.portal.ui.notify.controller.NotifyController
+import kotlinx.android.synthetic.main.fragment_notify.*
+import com.github.monolabhituszi.portal.ui.notify.SampleModel
 
 class NotifyFragment : Fragment(R.layout.fragment_notify) {
     private lateinit var binding: FragmentNotifyBinding
     private lateinit var notifyViewModel: NotifyViewModel
+    private lateinit var controller: NotifyController
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -20,11 +23,27 @@ class NotifyFragment : Fragment(R.layout.fragment_notify) {
 
         // view model
         notifyViewModel = ViewModelProvider(this).get(NotifyViewModel::class.java)
-        notifyViewModel.text.observe(viewLifecycleOwner, Observer {
-            binding.textNotify.text = it
-        })
 
         // lifecycle
         binding.lifecycleOwner = this
+
+        controller = NotifyController()
+
+        recycler_view.also {
+            it.setController(controller)
+        }
+
+        val sampleData = listOf<SampleModel>(
+            SampleModel(
+                title = "通知１",
+                description = "通知１だよー"
+            ),
+            SampleModel(
+                title = "通知2",
+                description = "通知2だよー"
+            )
+        )
+
+        controller.setData(sampleData)
     }
 }
---
+++ app/src/main/java/com/github/monolabhituszi/portal/ui/notify/SampleModel.kt
@@ -0,0 +1,3 @@
+package com.github.monolabhituszi.portal.ui.notify
+
+data class SampleModel(val title: String?, val description: String?)

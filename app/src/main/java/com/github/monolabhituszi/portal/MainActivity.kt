99bb0a2c66492fb57b249fb91e6b338f19a7c2bb
Moi28 <47969861+moi28@users.noreply.github.com>
2020-07-17 19:33:01 +0900

    add navigate to settings

--- app/src/main/java/com/github/monolabhituszi/portal/MainActivity.kt
+++ app/src/main/java/com/github/monolabhituszi/portal/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.github.monolabhituszi.portal
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.github.monolabhituszi.portal.databinding.ActivityMainBinding
@@ -19,4 +23,20 @@ class MainActivity : AppCompatActivity() {
                     as NavHostFragment).navController
         setupWithNavController(nav_view, navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_bar_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                    as NavHostFragment).navController
+        when(item.itemId){
+            R.id.action_bar_settings -> navController.navigate(R.id.navigation_settings)
+        }
+        return true
+    }
+
 }

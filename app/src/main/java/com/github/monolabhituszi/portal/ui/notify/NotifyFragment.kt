b58b4bb82b5699cc42c03df346b6330bca59758e
Moi28 <47969861+moi28@users.noreply.github.com>
2020-10-12 14:25:02 +0900

    fix databinding

--- app/src/main/java/com/github/monolabhituszi/portal/ui/notify/NotifyFragment.kt
+++ app/src/main/java/com/github/monolabhituszi/portal/ui/notify/NotifyFragment.kt
@@ -8,7 +8,6 @@ import com.github.monolabhituszi.portal.R
 import com.github.monolabhituszi.portal.databinding.FragmentNotifyBinding
 import com.github.monolabhituszi.portal.model.SampleNotifyModel
 import com.github.monolabhituszi.portal.ui.notify.controller.NotifyController
-import kotlinx.android.synthetic.main.fragment_notify.*
 
 class NotifyFragment : Fragment(R.layout.fragment_notify) {
     private lateinit var binding: FragmentNotifyBinding
@@ -29,7 +28,7 @@ class NotifyFragment : Fragment(R.layout.fragment_notify) {
 
         controller = NotifyController()
 
-        recycler_view.also {
+        binding.recyclerView.also {
             it.setController(controller)
         }
 

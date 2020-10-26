4d43184b54f637b35eef6fe258691e8eff932265
Yoshimasa <66538624+ys319@users.noreply.github.com>
2020-10-26 11:54:39 +0900

    change TaskFragment.kt onCreateView to onViewCreated.

--- app/src/main/java/com/github/monolabhituszi/portal/ui/task/TaskFragment.kt
+++ app/src/main/java/com/github/monolabhituszi/portal/ui/task/TaskFragment.kt
@@ -1,9 +1,7 @@
 package com.github.monolabhituszi.portal.ui.task
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -17,14 +15,17 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
     private lateinit var taskViewModel: TaskViewModel
     private lateinit var controller: TaskController
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentTaskBinding.inflate(inflater, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // binding
+        binding = FragmentTaskBinding.bind(view)
+
+        // view model
         taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+
+        // lifecycle
+        binding.lifecycleOwner = this
 
         controller = TaskController(
             object : TaskController.Listener {
@@ -52,6 +53,5 @@ class TaskFragment : Fragment(R.layout.fragment_task) {
         )
 
         controller.setData(sampleData)
-        return binding.root
     }
 }

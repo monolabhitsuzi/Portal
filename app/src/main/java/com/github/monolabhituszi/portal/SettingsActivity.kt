e17813410c29af7864aa679af2b9924beab8ab29
hrkk0 <66579023+hrkk0@users.noreply.github.com>
2020-11-03 00:10:31 +0900

    Update SettingsActivity.kt

--- app/src/main/java/com/github/monolabhituszi/portal/SettingsActivity.kt
+++ app/src/main/java/com/github/monolabhituszi/portal/SettingsActivity.kt
@@ -2,7 +2,6 @@ package com.github.monolabhituszi.portal
 
 import android.content.Context
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.preference.ListPreference

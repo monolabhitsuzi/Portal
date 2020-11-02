fe4061aa50b86b6fb60b7c1248a9c16c0c35eca3
Yoshimasa <66538624+ys319@users.noreply.github.com>
2020-11-02 12:44:07 +0900

    Add AttendanceFragment.

---
+++ app/src/main/java/com/github/monolabhituszi/portal/ui/attendance/AttendanceFragment.kt
@@ -0,0 +1,31 @@
+package com.github.monolabhituszi.portal.ui.attendance
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.github.monolabhituszi.portal.R
+import com.github.monolabhituszi.portal.databinding.FragmentAttendanceBinding
+import com.github.monolabhituszi.portal.model.SampleTaskModel
+import com.github.monolabhituszi.portal.ui.attendance.controller.AttendanceController
+
+class AttendanceFragment : Fragment(R.layout.fragment_attendance) {
+    private lateinit var binding: FragmentAttendanceBinding
+    private lateinit var attendanceViewModel: AttendanceViewModel
+    private lateinit var controller: AttendanceController
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // binding
+        binding = FragmentAttendanceBinding.bind(view)
+
+        // view model
+        attendanceViewModel = ViewModelProvider(this).get(AttendanceViewModel::class.java)
+
+        // lifecycle
+        binding.lifecycleOwner = this
+
+    }
+}

ab602b62bab98236b5409a00ce80bc935af2ea1c
Yoshimasa <66538624+ys319@users.noreply.github.com>
2020-12-21 14:44:17 +0900

    Fix import

--- app/src/main/java/com/github/monolabhituszi/portal/ui/home/HomeFragment.kt
+++ app/src/main/java/com/github/monolabhituszi/portal/ui/home/HomeFragment.kt
@@ -9,7 +9,7 @@ import com.github.monolabhituszi.portal.R
 import com.github.monolabhituszi.portal.databinding.FragmentHomeBinding
 // import com.github.monolabhituszi.portal.ui.home.controller.HomeController
 import com.google.android.material.tabs.TabLayoutMediator
-import java.util.*
+import java.util.Calendar
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
